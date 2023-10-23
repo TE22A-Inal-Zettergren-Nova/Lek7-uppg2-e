4c2b9a358e615fa06d2655d1c9a133f88efcf1b6
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-10-23 08:23:00 +0200

    klar

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner tb = new Scanner(System.in);
+        System.out.println("Hur gammal är du?");
+        int ålder = tb.nextInt();
+        System.out.println("Är du medlem? true/false");
+        boolean medlem = tb.nextBoolean();
+        
+
+        if(ålder>=18 || medlem==true){
+            System.out.println("Välkommen, du får komma in");
+        }
+        else{
+            System.out.println("Du får inte komma in");
+        }
     }
 }
